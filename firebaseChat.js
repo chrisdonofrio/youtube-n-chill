31a6dcb9338a2a605265a7414d38881871cb7669
Jameson <J.JacksonRPCV@gmail.com>
2015-12-17 20:10:22 -0500

    wired up firechat with firebase for full functionality. Now needs authentication

--- firebaseChat.js
+++ firebaseChat.js
@@ -1,5 +1,4 @@
-   
-      var chatRef = new Firebase('https://scorching-inferno-8276.firebaseio.com/');
+var chatRef = new Firebase("https://scorching-inferno-8276.firebaseio.com/");
       var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
       chatRef.onAuth(function(authData) {
         if (authData) {
@@ -12,6 +11,7 @@
           });
         }
       });
+ 
 
 
-    
+           
