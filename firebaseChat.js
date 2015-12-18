37625cbb882c6cadab6c7aecece556e47f1618c0
Jameson <J.JacksonRPCV@gmail.com>
2015-12-17 19:04:52 -0500

    changed basic firebase chat to Firechat. Not functioning, but connecting to Firebase

--- firebaseChat.js
+++ firebaseChat.js
@@ -1,40 +1,17 @@
-  // CREATE A REFERENCE TO FIREBASE
-  var messagesRef = new Firebase('https://dm01jxcs3hr.firebaseio-demo.com/');
+   
+      var chatRef = new Firebase('https://scorching-inferno-8276.firebaseio.com/');
+      var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
+      chatRef.onAuth(function(authData) {
+        if (authData) {
+          chat.setUser(authData.uid, "Anonymous" + authData.uid.substr(10, 8));
+        } else {
+          chatRef.authAnonymously(function(error, authData) {
+            if (error) {
+              console.log(error);
+            }
+          });
+        }
+      });
 
-  // REGISTER DOM ELEMENTS
-  var messageField = $('#messageInput');
-  var nameField = $('#nameInput');
-  var messageList = $('#example-messages');
 
-  // LISTEN FOR KEYPRESS EVENT
-  messageField.keypress(function (e) {
-    if (e.keyCode == 13) {
-      //FIELD VALUES
-      var username = nameField.val();
-      var message = messageField.val();
-
-      //SAVE DATA TO FIREBASE AND EMPTY FIELD
-      messagesRef.push({name:username, text:message});
-      messageField.val('');
-    }
-  });
-
-  // Add a callback that is triggered for each chat message.
-  messagesRef.limitToLast(10).on('child_added', function (snapshot) {
-    //GET DATA
-    var data = snapshot.val();
-    var username = data.name || "anonymous";
-    var message = data.text;
-
-    //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
-    var messageElement = $("<li>");
-    var nameElement = $("<strong class='example-chat-username'></strong>")
-    nameElement.text(username);
-    messageElement.text(message).prepend(nameElement);
-
-    //ADD MESSAGE
-    messageList.append(messageElement)
-
-    //SCROLL TO BOTTOM OF MESSAGE LIST
-    messageList[0].scrollTop = messageList[0].scrollHeight;
-  });
+    
