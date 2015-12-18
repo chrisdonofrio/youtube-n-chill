8ec551c14af8bca4e2e0e7eed86fa262a25ecb94
Jameson <J.JacksonRPCV@gmail.com>
2015-12-17 20:42:21 -0500

    resolved merge conflit. merged firechat to master

---
+++ firebaseChat.js
@@ -0,0 +1,14 @@
+
+var chatRef = new Firebase("https://scorching-inferno-8276.firebaseio.com/");
+      var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
+      chatRef.onAuth(function(authData) {
+        if (authData) {
+          chat.setUser(authData.uid, "Anonymous" + authData.uid.substr(10, 8));
+        } else {
+          chatRef.authAnonymously(function(error, authData) {
+            if (error) {
+              console.log(error);
+            }
+          });
+        }
+      });
