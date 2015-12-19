da797012690d54562a3d29424cf3eb7a241f76d1
jmona789 <jmona789@gmail.com>
2015-12-19 10:31:12 -0500

    added input box that will place iframe with youtube video

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
-}):
+  function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+  }
+
+  $(".addVideoUrlBtn").on("click", function(){
+    var videoId = youtube_parser($(".idInput").val());
+    var player = $("<iframe>");
+    player.attr("width", "560").attr("height", "315")
+    .attr("src", "https://www.youtube.com/embed/"+videoId)
+    .attr("frameborder", "0")
+    .attr("allowfullscreen");
+    $(".videoDiv").append(player);
+  })
+});
