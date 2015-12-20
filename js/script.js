19e965bc3ff967bd30514b165917e6d9440ceb0e
jmona789 <jmona789@gmail.com>
2015-12-19 21:39:12 -0500

    Got YouTube's iframe api working

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  var videoDB =  new Firebase("https://dazzling-fire-1875.firebaseio.com/");
 
   function youtube_parser(url){
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
@@ -6,6 +7,33 @@ $(document).ready(function() {
     return (match&&match[7].length==11)? match[7] : false;
   }
 
+
+  // create youtube player
+  var player;
+  function onYouTubePlayerAPIReady() {
+    player = new YT.Player('player', {
+      height: '315',
+      width: '560',
+      videoId: youtube_parser($(".urlStartVideoInput").val()),
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+
+  // autoplay video
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  // when video ends
+  function onPlayerStateChange(event) {        
+    if(event.data === 0) {            
+      alert('done');
+    }
+  }
+  /*
   $(".startVideoUrlBtn").on("click", function(){
     var videoId = youtube_parser($(".urlInput").val());
     var player = $("<iframe>");
@@ -17,4 +45,5 @@ $(document).ready(function() {
     $(".urlInput").hide();
     $(".startVideoUrlBtn").hide();
   })
+*/
 });
