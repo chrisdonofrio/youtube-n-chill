9b0e0c27590e75c087d4fa9fb7daaf4a3e44d937
Jameson <J.JacksonRPCV@gmail.com>
2015-12-23 10:51:28 -0500

    layed ground work of user authentication using Email/username. Firebase registering and login in working, but chat not initializing.

--- firebaseChat.js
+++ firebaseChat.js
@@ -1,17 +1,47 @@
-var chatRef = new Firebase("https://scorching-inferno-8276.firebaseio.com/");
-      var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
-      chatRef.onAuth(function(authData) {
-        if (authData) {
-          chat.setUser(authData.uid, "Anonymous" + authData.uid.substr(10, 8));
-        } else {
-          chatRef.authAnonymously(function(error, authData) {
-            if (error) {
-              console.log(error);
-            }
-          });
-        }
-      });
- 
+// Create a new Firebase reference, and a new instance of the Login client
+var isInitialized = false;
+var chatRef = new Firebase('https://scorching-inferno-8276.firebaseio.com/');
+chatRef.onAuth(function(authData) {
+// Once authenticated, instantiate Firechat with the user id and user name
+  if (authData && !isInitialized) {
+    initChat(authData);
+  }
+});
 
+// Create new user
+function registerUser() {
+  var username = document.getElementById("registerUsername").value;
+  var password = document.getElementById("registerPassword").value;
+  chatRef.createUser({
+    email    : username,
+    password : password
+  }, function(error, userData) {
+    if (error) {
+      console.log("Error creating user:", error);
+    } else {
+      console.log("Successfully created user account with uid:", userData.uid);
+    }
+  });
+}
 
-           
+// Login user
+function loginUser() {
+  var username = document.getElementById("loginUsername").value;
+  var password = document.getElementById("loginPassword").value;
+  chatRef.authWithPassword({
+    email    : username,
+    password : password
+  }, function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+  return false;
+}
+
+function initChat(authData) {
+  var chat = new FirechatUI(chatRef, document.getElementById('firechat-wrapper'));
+  chat.setUser(authData.uid, authData[authData.provider].displayName);
+}
