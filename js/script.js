129ba9401f78e2d0f5b0d199704d4bcbf206b97b
jmona789 <jmona789@gmail.com>
2015-12-28 03:55:30 -0500

    changed the way DB is structured and wrote code that will play the next video in queue after the current video ends

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ var database = new Firebase("https://dazzling-fire-1875.firebaseio.com/")
 var currentVideo = new Firebase("https://dazzling-fire-1875.firebaseio.com/currentVideo")
 var queuedVideos =  new Firebase("https://dazzling-fire-1875.firebaseio.com/queuedVideos");
 
-
-
+//takes YouTube URL and returns video ID
 function youtube_parser(url){
   var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
   var match = url.match(regExp);
   return (match&&match[7].length==11)? match[7] : false;
 }
-// create youtube player
+
+//create youtube player
 var player;
 function onYouTubePlayerAPIReady() {
   $(".startVideoUrlBtn").on("click", function(){
@@ -19,8 +19,8 @@ function onYouTubePlayerAPIReady() {
     if (videoId === false){
       $(".noTextAlertStart").slideDown().delay(1500).slideUp();
     }else{
-      var videoIdRef = database.child("currentVideo");
-      videoIdRef.push({
+      var currentVideoRef = database.child("currentVideo");
+      currentVideoRef.set({
         vidId: videoId,
         url: url
       })
@@ -54,23 +54,49 @@ function onPlayerReady(event) {
 }
 
 // when video ends
-function onPlayerStateChange(event) { 
+function onPlayerStateChange(event) {
+  //when video ends
   if(event.data === 0) {
+    $("iframe").attr("src", "");
+    $("iframe").remove();
+    $(".urlInput").show();
+    $(".startVideoUrlBtn").show();
   }
+  //if a video has been added to queued video list this will run otherwise it will not
+  queuedVideos.limitToFirst(1).on("child_added", function(snapshot) { 
+    if(event.data === 0) {
+      event.data = 1;
+      $(".urlInput").hide();
+      $(".startVideoUrlBtn").hide();
+      nextVideo = snapshot.val();
+      console.log(nextVideo)
+      currentVideo.set({
+        url: nextVideo.url,
+        vidId: nextVideo.vidId
+      });
+      var nextVideoInfoKey = snapshot.key();
+      var queuedRef = database.child("queuedVideos");
+      var nextVideoRef = queuedRef.child(nextVideoInfoKey);
+      nextVideoRef.remove();
+    }
+  })
 }
 
 $(document).ready(function() {
   var database = new Firebase("https://dazzling-fire-1875.firebaseio.com/")
   var queuedVideos =  new Firebase("https://dazzling-fire-1875.firebaseio.com/queuedVideos");
+  var currentVideo = new Firebase("https://dazzling-fire-1875.firebaseio.com/currentVideo")
+
   $(".videoAddedAlert").hide();
   $(".noTextAlertStart").hide();
   $(".noTextAlertAdded").hide();
 
  //pulls current video, if there is one, from DB and plays it
-  currentVideo.limitToFirst(1).on("child_added", function(snapshot) {
-    firstVideo = snapshot.val();
-    videoId = firstVideo.vidId;
-    console.log(firstVideo);
+  database.limitToFirst(1).on("child_added", function(snapshot) {
+    database = snapshot.val();
+    console.log(database);
+    videoId = database.vidId;
+    console.log(videoId);
     player = new YT.Player('player', {
       height: '315',
       width: '560',
@@ -86,15 +112,14 @@ $(document).ready(function() {
   })
 
   //removes current video and loads new one everytime current video is changed on DB
-  currentVideo.limitToFirst(1).on("child_changed", function(snapshot) {
+  currentVideo.limitToLast(1).on("child_changed", function(snapshot) {
     $("iframe").attr("src", "");
     $("iframe").remove();
     //creates new div for youtube API to replace with iframe
     newPlayerDiv = $("<div id='player'></div>");
     $(".videoDiv").prepend(newPlayerDiv);
     //creates new iframe of new current video
-    newVideo = snapshot.val();
-    videoId = newVideo.vidId;
+    videoId = snapshot.val();
     player = new YT.Player('player', {
       height: '315',
       width: '560',
@@ -122,8 +147,7 @@ $(document).ready(function() {
     }else{
       url = $(".urlQueueInput").val();
       videoId = youtube_parser($(".urlQueueInput").val());
-      var videoIdRef = database.child("queuedVideos");
-      videoIdRef.push({
+      queuedVideos.push({
         vidId: videoId,
         url: url
       });
