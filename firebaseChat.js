36735e5f2956f3b327187afbb3c86e7d5a32f3ca
Jameson <J.JacksonRPCV@gmail.com>
2016-01-01 16:14:47 -0500

    added buttons to initialize authentication process and basic sequence of showing and hiding form divs

--- firebaseChat.js
+++ firebaseChat.js
@@ -1,23 +1,37 @@
  // Create a new Firebase reference, and a new instance of the Login client
     var ref = new Firebase('https://scorching-inferno-8276.firebaseio.com/');
- 
-    // initialize the chat 
+
+    //Start: Two buttons. New user triggering the registration & login triggering email/password field
+    $(".login, .register").hide();
+    $("#logUser").click(function (){
+      $(".login").fadeIn(1500);
+    })
+
+    $("#registerUser").click(function(){
+      $(".register").fadeIn(1500);
+    })
+
+    // Initialize the chat 
     function initChat(authData) {
       var chat = new FirechatUI(ref, document.getElementById('firechat-wrapper'));
-      chat.setUser(authData.uid, authData.password.email.substr(0, authData.password.email.indexOf('@')));  //want to setUser to password.email, but it's not showing in object.
+      chat.setUser(authData.uid, authData.password.email.substr(0, authData.password.email.indexOf('@')));  
     }
 
     // Creating a user account 
     $("#registerAction").click(function(e) {
       event.preventDefault();
       ref.createUser({
-        email    : $("#registerUsername").val(),  //pulled from form
+        email    : $("#registerUsername").val(),  
         password : $("#registerPassword").val(),
       }, function(error, userData) {
         if (error) {
           console.log("Error creating user:", error);
+          //please use a valid name (no strange characters)
         } else {
           console.log("Successfully created user account with uid:", userData.uid);
+          //hide registration panel and show login panel
+          $(".registration").hide();
+          $(".login").show();
         }
       });
     })
@@ -26,7 +40,7 @@
     $("#loginAction").click(function(e) {
       event.preventDefault();
       ref.authWithPassword({
-        email    : $("#loginEmail").val(), //pulled from form
+        email    : $("#loginEmail").val(), 
         password : $("#loginPassword").val()
       }, function(error, authData) {
         if (error) {
@@ -34,6 +48,8 @@
         } else {
           console.log("Authenticated successfully with payload:", authData);
           initChat(authData);
+          $(".login, #logUser, #registerUser").hide();
+          //grab div with login panel and hide it
         }
       });
     })
