08df52c28c3e3c63cf673e7446a76c7b851cd12f
Jameson <J.JacksonRPCV@gmail.com>
2016-01-01 21:30:37 -0500

    moved login & signup button outside of panel for navbar. last push before moving forms to modal

--- firebaseChat.js
+++ firebaseChat.js
@@ -1,24 +1,19 @@
  // Create a new Firebase reference, and a new instance of the Login client
     var ref = new Firebase('https://scorching-inferno-8276.firebaseio.com/');
 
-    //Start: Two buttons. New user triggering the registration & login triggering email/password field
-    $("#regWarning, #loginWarning").hide();
-    $(".login, .register").hide();
-    $("#logUser").click(function (){
+    //hide forms until button click
+    $("#regWarning, #loginWarning, .login, .register").hide();
+    //on click show the right form
+    $("#signupButton").click(function (){
       $(".login").fadeIn(500);
+      $(".register").hide();
     })
-
-    $("#registerUser").click(function(){
+    $("#registerButton").click(function(){
       $(".register").fadeIn(500);
+      $(".login").hide();
     })
 
-    // Initialize the chat 
-    function initChat(authData) {
-      var chat = new FirechatUI(ref, document.getElementById('firechat-wrapper'));
-      chat.setUser(authData.uid, authData.password.email.substr(0, authData.password.email.indexOf('@')));  
-    }
-
-    // Creating a user account 
+    // New users: Creating a user account 
     $("#registerAction").click(function(e) {
       event.preventDefault();
       ref.createUser({
@@ -27,17 +22,17 @@
       }, function(error, userData) {
         if (error) {
           console.log("Error creating user:", error);
-          $("#regWarning").fadeIn(1200).fadeOut(800);
+          $("#regWarning").fadeIn(500).fadeOut(2400);
         } else {
           console.log("Successfully created user account with uid:", userData.uid);
           //hide registration panel and show login panel
-          $(".registration").hide();
+          $(".register").hide();
           $(".login").show();
         }
       });
     })
 
-    // Login user 
+    // Access input from form and respond to click by initializing chat
     $("#loginAction").click(function(e) {
       event.preventDefault();
       ref.authWithPassword({
@@ -46,12 +41,17 @@
       }, function(error, authData) {
         if (error) {
           console.log("Login Failed!", error);
-          $("#loginWarning").fadeIn(1200).fadeOut(800);
+          $("#loginWarning").fadeIn(500).fadeOut(2400);
         } else {
           console.log("Authenticated successfully with payload:", authData);
           initChat(authData);
-          $(".login, #logUser, #registerUser").hide();
-          //grab div with login panel and hide it
+          $(".login, #logUser, .register, #registerButton, #signupButton").hide();
         }
       });
     })
+
+    // Initialize the chat 
+    function initChat(authData) {
+      var chat = new FirechatUI(ref, document.getElementById('firechat-wrapper'));
+      chat.setUser(authData.uid, authData.password.email.substr(0, authData.password.email.indexOf('@')));  
+    }
